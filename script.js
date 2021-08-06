a27f3f0aae9de321fdc74529e888da65b7146ecc
Rupneil Saha Roy <72138183+rupneilsaharoy@users.noreply.github.com>
2021-08-06 20:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.querySelector("#menu-bar");
+var navbar = document.querySelector(".navbar");
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+});
+
